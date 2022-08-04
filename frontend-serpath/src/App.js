408c36f863ff00cd08f113e2b53a2db934a5b324
Michael <michaelbarr24@hotmail.com>
2022-08-04 12:43:21 +0100

    Add Basic Hero Section

--- frontend-serpath/src/App.js
+++ frontend-serpath/src/App.js
@@ -1,10 +1,12 @@
 import { Routes } from "react-router-dom";
+import Hero from "./components/Hero/Hero";
 import Nav from "./components/Nav/Nav";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Hero />
       <Routes></Routes>
     </div>
   );
---
+++ frontend-serpath/src/components/Hero/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Hero.scss"
+
+function Hero() {
+  return (
+    <section id='hero' className='container'>
+      <h1 className='hero-heading'>SerPath<span className='hero-subheading'>Web Development</span></h1>
+    </section>
+  )
+}
+
+export default Hero
