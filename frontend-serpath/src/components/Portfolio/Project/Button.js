c6d609375af800729d5fe6df91d1d17b80c9a6d2
Michael B <michaelbarr24@hotmail.com>
2022-08-06 22:42:27 +0100

    Update small fixes

--- frontend-serpath/src/components/Portfolio/Project/Button.js
+++ frontend-serpath/src/components/Portfolio/Project/Button.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./Button.scss";
-import { Link } from "react-router-dom";
 
 function Button(props) {
   const [active, setActive] = React.useState(false);
   let styles;
   React.useEffect(() => {
     props.url ? setActive(true) : setActive(false);
-  }, []);
+  }, [props.url]);
   if (!active) {
     styles = {
       opacity: 0.5,
