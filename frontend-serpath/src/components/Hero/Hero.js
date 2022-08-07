09c0f19718873c785f691be6627411d7df3637dc
Michael B <michaelbarr24@hotmail.com>
2022-08-07 09:02:53 +0100

    Add animation to hero

--- frontend-serpath/src/components/Hero/Hero.js
+++ frontend-serpath/src/components/Hero/Hero.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./Hero.scss";
+import TypeAnimation from "react-type-animation";
 
 function Hero() {
   return (
     <section id="hero" className="container">
-      <h1 className="hero-heading">
-        SerPath<span className="hero-subheading">Web Development</span>
+      <h1>
+        <span className="hero-heading">SerPath</span>
+        <TypeAnimation
+          cursor={true}
+          sequence={["Web Development", 5000]}
+          wrapper="span"
+          className="hero-subheading"
+        />
       </h1>
     </section>
   );
